ac8070b2fa4f8c34f76b4d12aa34d13a1157e8c1
Imane <imaneelmouradi98@gmail.com>
2019-07-31 10:11:12 +0100

    start project socket.io

---
+++ client.js
@@ -0,0 +1,42 @@
+const io = require("socket.io-client");
+
+const socket = io.connect("http://localhost:3000");
+
+function registerHandler(onMessageReceived) {
+  socket.on("message", onMessageReceived);
+}
+
+function unregisterHandler() {
+  socket.off("message");
+}
+
+socket.on("error", function(err) {
+  console.log("received socket error:");
+  console.log(err);
+});
+
+// socket.emit to emit custom events which our chat will listen to, As the second argument we can pass the actual data. 
+
+function register(name, cb) {
+  socket.emit("register", name, cb);
+}
+
+function join(chatroomName, cb) {
+  socket.emit("join", chatroomName, cb);
+}
+
+function leave(chatroomName, cb) {
+  socket.emit("leave", chatroomName, cb);
+}
+
+function message(chatroomName, msg, cb) {
+  socket.emit("message", { chatroomName, message: msg }, cb);
+}
+
+function getChatrooms(cb) {
+  socket.emit("chatrooms", null, cb);
+}
+
+function getAvailableUsers(cb) {
+  socket.emit("availableUsers", null, cb);
+}
